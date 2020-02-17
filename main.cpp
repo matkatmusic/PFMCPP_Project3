283818b6ea5058fb0d593839c9ebf7accef0d384
matkatmusic <matkatmusic@gmail.com>
2020-02-17 09:57:30 -0800

    Added Part2 Instructions

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
-/*
- Project 3 - Part 1 / 5
-Video:  Chapter 2 Part 5
- User-Defined Types
+ /*
+ Project 3 - Part 2 / 5
+ Video: Chapter 2 Part 6
+ Implementations tasks
+ 
+Create a branch named Part2
 
- Create a branch named Part1
+ tasks
+ 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
+    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
+    you should be able to deduce the return type of those functions based on their usage in Person::run()
+    You'll need to insert the Person struct from the video in the space below.
+ */
+
+
+
+
+
+ /*
+ 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
  
- 1) write 10 user-defined types, each with a random number of member variables
-    try to make the member variables have names that are related to the user-defined type.
+ 3) be sure to write the correct full qualified name for the nested type's member functions.
  
- 2) give the member variables relevant data types
- 
- 3) add a couple member functions.  
-    make the function parameter list for those member functions use some of your User-Defined Types
- 
- 4) make 2 of the 10 user-defined types be nested class
- 
- 5) One of your UDTs should only use UDTs for its member variable types.   
-     No primitives allowed!
- 
- 6) After you finish defining each type, click the [run] button.  Clear up any errors or warnings as best you can.
+ 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
@@ -26,27 +29,6 @@ Video:  Chapter 2 Part 5
 
  Wait for my code review.
  */
-
-/*
- example:
- */
-struct CarWash            //1) a U.D.T. with a random number of member variables
-{
-    int numSponges = 3;
-    double amountOfSoapUsedPerCar = 2.6; //2) relevant data types
-    unsigned int numCarsProcessed = 0;
-    
-    struct Car                             //4) nested class
-    {
-        bool isAPickupTruck = false;
-        float gasMileage = 26.2f;        //2) relevant data types
-    };
-    
-    void washAndWaxCar( Car car );         //3) member function with a user-defined type as the parameter.  The user-defined type parameter happens to be the nested class.
-    
-    Car myCar;  //5) a member variable whose type is a UDT.
-};
-
 
 /*
  1)
