365f5dc119eb616979f9020528a93746aa849d56
tristanmedjo <tristan.medjo@gmail.com>
2020-02-25 07:31:03 +0000

    Initial completion of Project 3 Part 1

--- main.cpp
+++ main.cpp
@@ -53,42 +53,144 @@ struct CarWash            //1) a U.D.T. with a random number of member variables
 /*
  1)
  */
+ struct Filter
+ {
+    double frequency = 4000.0;
+    int filterType = 2;
+    float dryWet = 1.0f;
 
+    struct FilterKnob
+    {
+        float knobPosition;
+        bool knobOverride = false;
+        int color = 1;
+    };
+
+    void updateKnobPosition(FilterKnob knobOne, double nextKnobPosition);
+};
 /*
  2)
  */
+ struct WavetableOscillator
+ {
+    double frequency = 4000.0;
+    int waveShape = 2;
+    float volumeLevel = 1.0f;
 
+    struct Wavetable
+    {   
+        int maxNumberOfWaveforms = 100;
+        bool interpolationOn = true;
+    };
+
+    int getNextFrequencyInSequence(float nextFrequency);
+ };
 /*
  3)
  */
+    struct ADSREnvelope
+    {
+        double attack  = 2.0;
+        double decay = 2.0;
+        float sustain = 1.f;
+        float release =2.4f;
+        bool isMidiControlEnabled = true;
 
+        void playNoteOnTrigger(ADSREnvelope myEnvelope);
+    };
 /*
  4)
  */
+struct Reverb
+{
+    int decayTime;
+    float roomSize;
+    double inputVolume;
+    double outputVolume;
 
+    int setNextReverbSettings(Reverb defaultVerbSettings);
+};
 /*
  5)
  */
-
+struct Equalizer
+{
+    double frequency = 2000.19;
+    float equalizerQ = 1.f;
+    float gain = 0.5f;
+    
+    void disableEqualizer(Equalizer defaultEQ);
+};
 /*
  6)
  */
+struct Delay
+{
+    bool bpmSync = false;
+    float feedback = 50.f;
+    int delayRate = 1;
+    bool pingPong = false;
 
+    void copySettingsToAllOfSameType(Delay defaultDelay, bool shouldOverride);
+};
 /*
  7)
  */
+struct Synthesizer
+{
+    WavetableOscillator oscillator;
+    Filter combFilter;
+    ADSREnvelope defaultEnvelope;
 
+    void playSound(Synthesizer mySynth, float durationLength, bool pitchGlideEnabled);
+};
 /*
  8)
  */
+struct SimpleLooper
+{
+    float loopStartPoint = 0.f;
+    float loopEndPoint = 10.f;
+    bool shouldLoop = true;
 
+    SimpleLooper(int audioClipList);
+};
 /*
  9)
  */
+struct Bank
+{
+    float totalMoney = 100000000.f; // 100,000,000
+    bool canLoanMoney = true;
+    float yearlyInterestRate = 4.0f;
+    
+    struct PersonalAccount
+    {
+        float valueOfAccount = 50000.f;
+        bool owesMoney = true;
+        int totalAssetsHeld = 15;
+    };
 
+    void payOffLoans(Bank chase, PersonalAccount newAccount);
+};
 /*
  10)
  */
+struct SearchEngine
+{
+    int totalSearches = 50;
+    int totalAdsBlocked = 100;
+    bool accountLoggedIn = false;
+
+    struct SearchBar
+    {
+        float widthOfSearchBar = 100.f;
+        float heightOfSearchBar = 5.f;
+        bool searchEnabled = true;
+    };
+
+    SearchEngine(char recentlySearchedAddress);
+};
 
 #include <iostream>
 int main()
