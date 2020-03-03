25aeecd83083e7db387e94bf0eed55b24a744706
matkatmusic <matkatmusic@gmail.com>
2020-03-02 20:34:23 -0800

    added part3 instructions

--- main.cpp
+++ main.cpp
@@ -1,75 +1,56 @@
- /*
- Project 3 - Part 2 / 5
- Video: Chapter 2 Part 6
- Implementations tasks
+/*
+ Project 3 - Part 3 / 5
+ video: Chapter 2 - Part 8
+ Constructors tasks
+
+ Create a branch named Part3
+
+ On a new branch:
+
+ 1) Add a constructor for each User-Defined-Type.
  
-Create a branch named Part2
-
- tasks
- 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
-    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
-    you should be able to deduce the return type of those functions based on their usage in Person::run()
-    You'll need to insert the Person struct from the video in the space below.
- */
-
-struct Person
-{
-    int age;
-    int height;
-    float hairLength;
-    float GPA;
-    unsigned int SATScore;
-    int distanceTraveled; 
-
-    struct Foot
-    {
-        void stepForward()
-        {
-            //step forward function
-        }
-        int stepSize()
-        {
-           return{}; //step size function
-        }
-    };
-    
-    void run(bool startWithLeftFoot);
-
-    Foot leftFoot, rightFoot;
-
-};
-
-void Person::run(bool startWithLeftFoot)
-{
-    if( startWithLeftFoot)
-    {
-        leftFoot.stepForward();
-        rightFoot.stepForward();
-    }
-    else 
-    {
-        rightFoot.stepForward();
-        leftFoot.stepForward();
-    }
-    distanceTraveled += leftFoot.stepSize() + rightFoot.stepSize();
-}
-
-
- /*
- 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
+ 2) Instantiate 1 or 2 instances of each of your user-defined types in the main function at the bottom of that file, and call some of those member functions.
  
- 3) be sure to write the correct full qualified name for the nested type's member functions.
+ 3) make them print out something interesting via std::cout
  
- 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ After you finish defining each type/function:
+ click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+ Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
 send me a DM to check your pull request
 
  Wait for my code review.
+ 
+ example:
  */
+
+#include <iostream>
+namespace Example 
+{
+struct UDT  // my user defined type
+{
+    int a; //a member variable
+    UDT() { a = 0; }             //3) the constructor
+    void printThing()            //1) the member function
+    {
+        std::cout << "UDT::printThing() " << a << std::endl;  //5) printing out something interesting
+    }
+};
+
+int main()
+{
+    UDT foo;              //4) instantiating a UDT in main()
+    foo.printThing();     //4) calling a member function of the instance that was instantiated.
+    
+    return 0;
+}
+}
+
+//insert Example::main() into main() of user's repo.
+
 
 
 /*
@@ -384,5 +365,6 @@ void ConcertHall::addInstrumentToStage (Piano)
 #include <iostream>
 int main()
 {
+    Example::main();
     std::cout << "good to go!" << std::endl;
 }
