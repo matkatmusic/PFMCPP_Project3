69b5694dd4f43e13d3f85cb53dac479fc59a456c
Jerry Lee Melton <jerryleemelton@gmail.com>
2020-04-08 00:13:08 +0000

    Fleshed out code for UDTs.

--- main.cpp
+++ main.cpp
@@ -176,17 +176,36 @@ struct CarWash //2)
 1)
  */
 
-struct Lizard
+struct Chameleon
 {
+    struct Color
+    {
+        int red;
+        int green;
+        int blue;
+        int alpha;
+
+        void setColor(int newRedValue, int newGreenValue, int newBlueValue);
+        void setAlpha(int newAlphaValue);
+    };
+
     // 1) Length
+    float length;
     // 2) Weight
+    float weight;
     // 3) Color
-    // 4) Teeth
+    Color color;
+    // 4) Number of Teeth
+    int numberOfTeeth;
     // 5) Scales
+    int numberOfScales;
 
     // 1) Run
-    // 2) Eat
+    void run(float speed, float distanceToTravel);
+    // 2) Change Color
+    void changeColor(int newRedValue, int newGreenValue, int newBlueValue);
     // 3) Flick Tongue
+    void flickTongue(float flickDistance, int numberOfFlicks);
 };
 
 
@@ -194,17 +213,27 @@ struct Lizard
 2)
  */
 
-struct AudioInterface
+struct FastFoodRestaurant
 {
-    // 1) Inputs
-    // 2) Outputs
-    // 3) Sample rate
-    // 4) Preamps
-    // 5) USB Port
+    // 1) Deep fryers
+    int numDeepFryers;
+    // 2) Cash registers
+    int numCashRegisters;
+    // 3) Profits per week
+    float profitsPerWeek;
+    // 4) Fries used per week
+    int potatoesUsedPerWeek;
+    // 5) Burger patties in freezer
+    int burgersInFreezer;
 
-    // 1) Output audio
-    // 2) Receive Input
-    // 3) Convert digital data to analog signal
+    // 1) Make burger
+    bool makeBurger(int numBurgers);
+    // 2) Make fries
+    bool makeFries(int potatoesToUse);
+    // 3) Charge customer
+    float chargeCustomer();
+    // 4) Make special chameleon burger
+    bool makeChameleonBurger(Chameleon chameleon);
 };
 
 /*
@@ -213,15 +242,23 @@ struct AudioInterface
 
 struct ElectricGuitar
 {
-    // 1) Body
-    // 2) Neck
-    // 3) Bridge
-    // 4) Pickups
-    // 5) Electronics
+    // 1) Neck length
+    float neckLength;
+    // 2) Volume knob position
+    float volumeKnobPosition;
+    // 3) Tone knob position
+    float toneKnobPosition;
+    // 4) Number of pickups
+    int numberOfPickups;
+    // 5) Number of strings
+    int numberOfStrings;
 
     // 1) Output sound
+    void outputSound();
     // 2) Increase/decrease volume
+    void setVolume(float newVolumePosition);
     // 3) Adjust tone
+    void setTone(float newTonePosition);
 };
 
 /*
@@ -230,15 +267,37 @@ struct ElectricGuitar
 
 struct TapePlayer
 {
-    // 1) Tape Slot
+    struct Tape
+    {
+        float length;
+        float currentPosition;
+        bool isWritable;
+
+        void setWritable(bool writable);
+        void updatePosition(float newPosition);
+    };
+
+
+    // 1) Tape
+    Tape tape;
     // 2) Control Buttons
-    // 3) Power Button
+    int numberOfButtons;
+    // 3) Tape timer position
+    float tapeTimerPosition;
     // 4) Speed Selector
-    // 5) Headphone jack
+    float speedSelectorPosition;
+    // 5) Volume Slider
+    float volumeSliderPosition;
+
 
     // 1) Play tape
+    void playTape(Tape& tape, float lengthToPlay, float speedSelectorPosition);
     // 2) Rewind tape
-    // 3) Eject Tape
+    void rewindTape(Tape& tape);
+    // 3) Record to tape
+    bool recordToTape(Tape& tape);
+    // 4) Reset tape timer
+    bool resetTapeTimer();
 };
 
 /*
@@ -248,14 +307,22 @@ struct TapePlayer
 struct Display
 {
     // 1) Number of pixels
+    int numberOfPixels;
     // 2) Brightness
+    float brightness;
     // 3) Width
+    float widthInInches;
     // 4) Height
+    float heightInInches;
     // 5) Amount of power consumed
+    float powerConsumedInWatts;
 
     // 1) Update pixels with image information
+    void updatePixels();
     // 2) Clear pixels
+    void clearPixels();
     // 3) Adjust brightness 
+    void setBrightness(float newBrightnessValue);
 };
 
 /*
@@ -265,31 +332,47 @@ struct Display
 struct WiFiAdapter
 {
     // 1) Frequency
+    float frequency;
     // 2) Upload speed
+    float uploadSpeed;
     // 3) Download speed
+    float downloadSpeed;
     // 4) Channel
+    int channel;
     // 5) Amount of power consumed
+    float powerConsumedInWatts;
 
     // 1) Connect to an access point
+    void connectToAccessPoint();
     // 2) Upload data 
+    void uploadData();
     // 3) Download data
+    void downloadData();
 };
 
 /*
 7)
  */
 
-struct audioOutputSystem
+struct AudioOutputSystem
 {
     // 1) Volume level
+    float volumeLevel;
     // 2) Sample rate
+    int sampleRate;
     // 3) Bit depth
+    int bitDepth;
     // 4) number of output channels
+    int numberOfOutputChannels;
     // 5) amount of power consumed
+    float powerConsumedInWatts;
 
     // 1) Output audio
+    void outputAudio();
     // 2) Receive audio input
+    void receiveAudioInput();
     // 3) Adjust volume level
+    void setVolumeLevel();
 };
 
 /*
@@ -299,14 +382,22 @@ struct audioOutputSystem
 struct CPU
 {
     // 1) Clock speed in MHz
+    float clockSpeedInMHz;
     // 2) Number of cores
+    int numberOfCores;
     // 3) Number of threads
+    int numberOfThreads;
     // 4) Cache size in MB
+    float cacheSizeInMB;
     // 5) amount of power consumed
+    float powerConsumedInWatts;
 
     // 1) Fetch instructions from memory
+    void fetchInstructions(int threadNumber, int sizeOfInstructions);
     // 2) Execute instructions
+    void executeInstructions(int threadNumber);
     // 3) Write data to memory
+    void sendDataToRAM();
 };
 
 /*
@@ -316,14 +407,22 @@ struct CPU
 struct RAM
 {
     // 1) Capacity 
+    int capacityInBytes;
     // 2) Clock speed
+    float clockSpeedInMHz;
     // 3) Bus width
+    int busWidthInBits;
     // 4) Number of pins
+    int numPins;
     // 5) Amount of power consumed
+    float powerConsumedInWatts;
 
     // 1) Write data to memory
+    bool writeToMemory(int dataToWrite);
     // 2) Clear memory
+    void clearMemory();
     // 3) Send data to CPU
+    void sendDataToCPU();
 };
 
 /*
@@ -333,14 +432,22 @@ struct RAM
 struct PortableVideoGameSystem
 {
     // 1) Display
+    Display display;
     // 2) WiFi Adapter
+    WiFiAdapter wifiAdapter;
     // 3) Audio output system
+    AudioOutputSystem audioOutput;
     // 4) CPU
+    CPU cpu;
     // 5) RAM
+    RAM memory;
 
     // 1) Load game software
+    void loadSoftware();
     // 2) Show images on display
+    void sendImageToDisplay();
     // 3) Play sound from speakers
+    void sendSoundToSpeakers();
 };
 
 #include <iostream>
