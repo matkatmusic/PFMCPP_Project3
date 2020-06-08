89c9b63a7f19fdd555a79f50f935862a84a5ecba
jasonp827 <jasonp827@gmail.com>
2020-06-08 18:40:53 +0000

    Finished 1e

--- main.cpp
+++ main.cpp
@@ -72,6 +72,7 @@ Thing: Car Wash
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 struct CarWash //                                   1) define an empty struct for each of your 10 types.       
 {
@@ -113,12 +114,243 @@ struct CarWash //                                   1) define an empty struct fo
     Car carBeingServiced;  
 };
 
+struct Prophet6
+{
+    //1) number of oscillators
+    int numOscillators = 12;
+    //2) FX on/off
+    bool fxOnOff = true;
+    //3) filter cutoff amount
+    float filterCutoff = 0.5f;
+    //4) distortion amount
+    float distortion = 0.01f;
+    //5) keyboard notes (unison)
+    int note = 32;
+
+    struct Globals
+    {
+        //1) transpose value
+        int transpose = 0;
+        //2) master tune value
+        int masterTune = 0;
+        //3) midi channel
+        int midiChannel = 1;
+        //4) midiClock
+        int midiClock = false;
+        //5) clock port (MIDI or USB)
+        bool clockPort = false;
+    };
+
+    //1) modulate cutoff
+    void modCutoff(float filterCutoff);
+    //2) play sequencer
+    void playSequence(int note);
+    //3) turn on FX
+    bool engageFX(bool fxOnOff);
+};
+
+struct Tr808
+{
+    //1) number of voices
+    int numVoices = 11;
+    //2) step trigger
+    bool stepTrig = true;
+    //3) level amount per voice in decibels
+    float voiceLevel = -2.3f;
+    //4) preset number
+    int preset = 12;
+    //5) tempo bpm
+    int tempo = 135;
+
+    //1) program sequence using an array of engaged steps as an argument
+    void programSequence(std::vector<bool> sequence = {true, false, false, false}, bool stepTrig = true);
+    //2) adjust tone
+    float adjustTone(float knobTurn);
+    //3) change the world
+    double changeTheWorld(bool musicIsAmazing);
+};
+
+struct ElectricGuitar
+{
+    //1)  volume level in decibels
+    float volume = -0.45f;
+    //2)  tone level
+    float tone = 12.2f;
+    //3)  number of strings
+    int strings = 6;
+    //4)  tuning pegs rotation degree
+    double tunePeg = 23.43;
+    //5)  seymour duncan pickups
+    bool seymourDuncanPickups = true;
+
+    //1)  play music
+    bool playMusic(double tunePeg, float volume = 0.0f, int strings = 6);
+    //2)  feedback
+    float feedback(float volume, float tone, float ampGain);
+    //3)  guitar spin
+    bool guitarSpin(bool crowdIsWild);
+};
+
+struct GranularSynth
+{
+    //1)  grain size in seconds
+    float grainSize = 0.6f;
+    //2)  grain density(amount of grains played at once)
+    int grainDensity = 7;
+    //3)  playback position
+    float playbackPosition = 0.0;
+    //4)  varispeed of sample
+    float varispeed = 1.0f;
+    //5)  random
+    double random = 0.12;
+
+    //1)  granulate
+    void granulate(float grainSize, int grainDensity, float playbackPosition, float varispeed, double random);
+    //2)  change playback position
+    void slide(float playBackPosition, float modulationInput);
+    //3)  modulate grain size
+    void modGrainSize(float grainSize, float modulationInput);
+};
+
+struct Oscillators
+{
+    //1)  -12v power consumption in mA
+    int negativeTwelvePower = 40;
+    //2)  +12v power consumption in mA
+    int positiveTwelvePower = 30;
+    //3)  waveshape value
+    float waveshape = 12.4f;
+    //4)  output type (sine, saw, waveshaper)
+    int outputType = 2;
+    //5)  FM attenuator value
+    float fmAttenuator = 0.46f;
+
+    //1)  outputs wave
+    bool outputWave(int negativeTwelvePower, int positiveTwelvePower);
+    //2)  hard sync
+    void hardSync(float inputSignal, bool hardSyncOnOff);
+    //3)  receives pitch CV
+    void receivePitchCV(float inputVoltage, float fmAttenuator);
+};
+
+struct Filters
+{
+    //1)  filter type (HP, LP, BP)
+    int filterType = 1;
+    //2)  FM attenuator value
+    float fmAttenuator = 43.23f;
+    //3)  cutoff value
+    float cutoff = 56.23f;
+    //4)  resonance value
+    float resonance = 15.54f;
+    //5)  gain value +dB
+    float gain = 12.3f;
+
+    //1)  filter audio
+    void filter(int filterType, float fmAttenuator, float cutoff, float resonance, float gain);
+    //2)  track 1v/oct
+    void trackPitch(float cutoff, float fmAttenuator);
+    //3)  change filter cutoff
+    void modFilter(float cutoff, float fmAttenuator);
+};
+
+struct Envelopes
+{
+    //1)  attack time in ms
+    int attack = 243;
+    //2)  decay time in ms
+    int decay = 501;
+    //3)  sustain level in percentage
+    float sustain = 0.67f;
+    //4)  release time in ms
+    int release = 684;
+    //5)  hold time in ms
+    int hold = 43;
+
+    //1)  output CV
+    float outputCV(bool powerOn, bool gateHigh);
+    //2)  receive gate/trigger
+    bool receive(bool cycleModeOnOff = false);
+    //3)  engage cycle mode
+    bool cycleMode(bool switchPosition);
+};
+
+struct VCAs
+{
+    //1)  number of audio inputs
+    int audioInputs = 4;
+    //2)  number of cv inputs
+    int cvInputs = 4;
+    //3)  cascading cv inputs
+    bool cascadingCV = true;
+    //4)  boost switch
+    bool boostSwitch = true;
+    //5)  number of audio outputs
+    int audioOutputs = 4;
 
 
+    //1)  receive cv
+    void receiveCV(int cvInputs);
+    //2)  output audio/cv
+    void output();
+    //3)  mute output
+    void mute(bool muteSwitch);
+};
 
+struct CVSequencer
+{
+    //1)  number of channels
+    int cvChannels = 8;
+    //2)  channel output level
+    float outputLevel = 2.3f;
+    //3)  number of steps
+    int steps = 16;
+    //4)  clock mode(slave or master)
+    bool clockMode = false;
+    //5)  scale select
+    int scale = 2;
 
+    //1)  output cv
+    bool output(bool powerOn);
+    //2)  receive clock
+    bool receiveClock(bool clockMode);
+    //3)  arpeggiate steps
+    bool arp(bool stepsOn, std::vector<int> stepOrder[]);
+};
 
+struct ModularSynth
+{
+    //1)  oscillators
+    Oscillators oscillators;
+    //2)  filters
+    Filters flters;
+    //3)  envelopes
+    Envelopes envelopes;
+    //4)  VCAs    
+    VCAs vcas;
+    //5)  CV Sequencer
+    CVSequencer cvsequencer;
 
+    struct Module
+    {
+        int negativeTwelvePower;
+        int positiveTwelvePower;
+        int hp;
+        bool threeU = true;
+        int numberOfInputs;
+
+        void outputVoltage(int negativeTwelvePower, int positiveTwelvePower);
+        void receiveVoltage();
+        void processVoltage(float inputValues);
+    };
+
+    //1)  changes pitch
+    void changePitch(void receivePitchCV(float inputVoltage, float fmAttenuator));
+    //2)  cross modulates
+    void crossMod(Envelopes);
+    //3)  modulate decay time of AD envelope
+    void modDecayEnv(int decay);
+};
 
 /*
 Thing 1) Prophet-6
