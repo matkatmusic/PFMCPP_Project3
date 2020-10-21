de67aa69311f772b66d85d28fb38cf21220d9ac1
matkatmusic <matkatmusic@gmail.com>
2020-10-21 16:33:10 -0700

    added part 1c

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-Project 3 - Part 1b / 5
+Project 3 - Part 1c / 5
 Video:  Chapter 2 Part 5
 User-Defined Types
 
@@ -20,37 +20,36 @@ You are going to write 10 UDTs in project3.
     Part 1e: you will convert those 10 plain-english UDTs into code that runs.
 ************************
 
-1)  Fill in #1 - 4 with a random UDT in plain english
-    These 4 UDTs do not need to be related.
-        a) For each plain-english UDT, write out 5 traits or properties and 3 things it can do
-        b) remember: these properties will eventually become primitives
-*/
+1) write the 10th UDT
+    come up with an object that is made of 5 smaller parts.
+    These parts will not be defined using Primitives, but instead will be their own UDTs you'll define in Part 1d
+    
+    Here is an example:
+    Cell Phone
 
-/*
- example:  
- 
-Thing: Car Wash   
-    5 properties:
-        - number of vacuum cleaners
-        - number of eco-friendly cleaning supplies
-        - stores the amount of water used per week.
-        - stores amount of profit made per week
-        - number of cars serviced per day
-    3 things it can do:
-        - wash and wax car
-        - charge customer
-        - detail the car interior
+    A Cell Phone is made up of the following 5 properties:
+        Display
+        Memory
+        CPU
+        Radio
+        Applications
 
-    Notice that I did not use "has a vacuum cleaner" or "Has eco-friendly cleaning supplies" as one of the properties
+    A Cell Phone has 3 things it can do:
+        make a call
+        send a text
+        run an application.
+
+    Notice that I did not use "has a display" or "Has memory" or "has a cpu" as one of the properties of the CellPhone
     
     Writing 'has a ___" checks whether or not your object **has the ability to do something**.
-    Instead, I wrote "number of vacuum cleaners" and "number of eco-friendly cleaning supplies".  
-    
-    These are specific objects or amounts. 
+    Instead, I wrote "Display" or "CPU".  These are specific objects or amounts. 
     
     In C++ terms, this means to I want you to avoid using 'bool' (has a) as a member variable type.
     Instead, prefer the other primitive types.
- */
+
+    When you choose your 5 smaller parts, remember that each of these 5 Sub Objects will need 
+    to be defined with 5 primitive properties and 3 actions EACH.  
+*/
 
 /*
 Thing 1) shoe
@@ -108,7 +107,19 @@ Thing 4) city
     3) build public transportation
  */
 
-
+/*
+Thing 10)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
