d1d008fb962c2dca22780ca1dcf1a96e0f9161a9
KottV <kill_it@mail.ru>
2021-01-20 22:09:16 +1000

    Add subobjects and actions

--- main.cpp
+++ main.cpp
@@ -36,51 +36,53 @@ Sub Object 1: Steering Wheel
     Name 4 things you'll find on the:    Steering Wheel
         1) paddle shifters
         2) 'cruise control' controls
-        3) 
-        4) 
+        3) horn button
+        4) volume control
     Name 2 things you can do with the:   Steering Wheel
         1) adjust cruise control settings.
-        2)
+        2) shift transmission
+        3) adjust volume
+        4) make a beep
         
 Sub Object 2: Instrument Cluster
     Name 4 things you'll find on the:   Instrument Cluster
-        1)
-        2)
-        3)
-        4)
+        1) speedometer
+        2) tachometer
+        3) outdoor thermometer
+        4) fuel level indicator
     Name 3 things you can do with the:   Instrument Cluster
-        1)
-        2)
-        3)
+        1) check the speed
+        2) check RPM
+        3) check fuel level
     
 Sub Object 3: Environment Controls
     Name 3 things you'll find on the:    Environment Controls
-        1)
-        2)
-        3)
+        1) heater control
+        2) AC control
+        3) airflow control
     Name 3 things you can do with the:   Environment Controls
-        1)
-        2)
-        3)
+        1) adjust temperature
+        2) toggle AC
+        3) switch airflow source
 
 Sub Object 4: Infotainment System
     Name 3 things you'll find on the:    Infotainment System
-        1)
-        2)
-        3)
+        1) navigation map
+        2) music player
+        3) AC indicator
     Name 3 things you can do with the:   Infotainment System
-        1)
-        2)
-        3)
+        1) set the route
+        2) play music
+        3) check AC status
 
 Sub Object 5: Seat 
     Name 3 things you'll find on the:    Seat
-        1)
-        2)
-        3)
+        1) seatbelt
+        2) headrest
+        3) lost coin
     Name 2 things you can do with the:   Seat
-        1)
-        2)
+        1) fasten seatbelt
+        2) adjust headrest
 */
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
