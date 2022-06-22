773bc41a0efb3ad1533f2acc1c695f2388a5710f
matkatmusic <matkatmusic@gmail.com>
2022-06-21 19:17:41 -0700

    Added part 2

--- main.cpp
+++ main.cpp
@@ -1,85 +1,18 @@
+ /*
+ Project 3 - Part 2 / 5
+ Video: Chapter 2 Part 6
+ Implementations tasks
+ 
+Create a branch named Part2
+
+ tasks
+ 0) delete all of the plain english pseudo-code you added in Part1.
+   don't forget to remove the blank lines left behind after you remove your comments
+   - you should be left with only your UDTs.
+*/
+// example:
+// if you had something like this at the end of Part1e:
 /*
-Project 3 - Part 1e / 5
-Video:  Chapter 2 Part 5
-User-Defined Types
-
-Continue your work on branch Part1
-
-Purpose: The entire purpose of this 5-part project is to get you writing C++ code that compiles and to 
-reinforce the syntax habits that C++ requires.  
-What you create in this project will be used as the basis of Project 5 in the course.   
-
-************************
-Part1 purpose:  Learn to write UDTs
-
-You are going to write 10 UDTs in project3.  
-Part1 will be broken up into 5 separate steps
-    Part 1a: you will learn to think about an object in terms of its sub-objects.
-    Part 1b: you will write 4 un-related UDTs in plain english
-    Part 1c: you will write 1 UDT in plain english that will be made of 5 related sub-objects
-    Part 1d: you will write plain-english UDTs for the 5 sub-objects that form the UDT defined in Part 1c
-    Part 1e: you will convert those 10 plain-english UDTs into code that runs.
-************************
-
-Convert your 10 Plain-english UDTs into code.
-
-I recommend compiling after finishing each one and making sure it compiles 
-without errors or warnings before moving on to writing the next UDT. 
-
-1) define an empty struct below your plain-english UDT. i.e.:
-
-Thing: Car Wash   
-    5 properties:
-        - number of vacuum cleaners
-        - number of eco-friendly cleaning supplies
-        - stores the amount of water used per week.
-        - stores amount of profit made per week
-        - number of cars serviced per day
-    3 things it can do:
-        - wash and wax car
-        - charge customer
-        - detail the car interior
- */
-
-#if false //ignore these #if #endif lines. they're just here to prevent compiler errors.
-struct CarWash
-{
-
-};
-#endif
-/*
-    - Do this for all 10 UDTs
-
-2) Below your plain-english UDT, Copy your 5 properties & 3 actions into the empty struct body.
-    - comment them out.
-    - Do this for all 10 UDTs
-    
-3) declare your member variables and member functions underneath each plain-english comment in your struct's body.
-    - give the member variables relevant data types
-    - Do this for all 10 UDTs
-    - if your functions return something other than 'void', add a comment explaining what is being returned.  see the example code below.
- 
-4) make the function parameter list for those member functions use some of your User-Defined Types
-    - You'll write definitions/implementations for these functions in Project3 Part2
-    - you'll call each of these functions in Project3 part3
-    - Do this for all 10 UDTs
- 
-5) make 2 of the 10 user-defined types have a nested class.  
-    - this nested class also needs 5 properties and 3 actions.
-    - these nested classes are not considered one of your 10 UDTs.
-    - this nested class must be related to the class it is nested inside
- 
-6) your 10th UDT's properties should be instances of your #5-#9 UDTs.   
-    - No primitives allowed!
- 
-7) After you finish defining each type, click the [run] button.  
-    Clear up any errors or warnings as best you can. 
-    if your code produces a [-Wpadded] warning, add '-Wno-padded' to the .replit file with the other compiler flags (-Weverything -Wno-missing-prototypes etc etc)
- */
-
-/*
- example:  
-
 Thing: Car Wash   
     5 properties:
         - number of vacuum cleaners
@@ -95,11 +28,12 @@ Thing: Car Wash
 
 #include <iostream>
 #include <string>
-
-struct CarWash //                                   1) define an empty struct for each of your 10 types.       
+namespace Part1eVersion 
 {
-    //number of vacuum cleaners                     2) copied and commented-out plain-english property
-    int numVacuumCleaners = 3; //                   3) member variables with relevant data types.
+struct CarWash        
+{
+    //number of vacuum cleaners                     
+    int numVacuumCleaners = 3; 
     //number of eco-friendly cleaning supplies      
     int numEcoFriendlyCleaningSupplies = 20;     
     //stores the amount of water used per week.     
@@ -109,82 +43,90 @@ struct CarWash //                                   1) define an empty struct fo
     //number of cars serviced per day               
     int numberOfCarsServiced = 10;               
     
-    struct Car //5)                                 Note that the nested type 'Car' is related to the 'CarWash' 
+    struct Car  
     {
-        //2) member variables with relevant data types.  the names are appropriate for the U.D.T.'s purpose.
         bool isAPickupTruck = false;
         float gasMileage = 26.2f;        
         int year = 1985;
         std::string manufacturer = "Toyota";
         std::string model = "Corolla";
 
-        //3) a member function.  it has multiple arguments, some with default values.
-        //the parameter names are related to the work the function will perform.
         void fillTank(float costPerGallon, double fuelAmountInGallons = 2.0, bool requiresDiesel = false);  
         void breakDown(std::string failureType, bool requiresTow = false);
-        int getMilesTraveledAnnually(bool includeUberLyftTrips); //3) returns the number of miles traveled
+        int getMilesTraveledAnnually(bool includeUberLyftTrips);
     };
 
     //wash and wax car
-    void washAndWaxCar(Car carA); //4) a member function whose parameter is a UDT.
+    void washAndWaxCar( Car car ); 
     //charge customer
-    float chargeCustomer(float discountPercentage); //3) returns the total amount charged.
+    float chargeCustomer(float discountPercentage);
     //detail the car interior
-    void detailInterior(Car carB);
+    void detailInterior( Car car );
     
-    //5) a member variable whose type is a UDT.
     Car carBeingServiced;  
-
-    /*
-    Pay attention to the member functions that take an instance of 'Car'
-    Notice that there is a member variable of the same type.
-
-    It makes sense to pass a Car to the function 'washAndWaxCar' because car washes service MANY cars
-    However, they only service ONE car at a time.
-    the carBeingServiced's value would change every time you wash and wax the car. 
-
-    I see many students who write code emulating this format, but their usage does not make logical sense.  
-    Consider the following snippet:
-
-    struct SoccerTeam
-    {
-        struct Manager
-        {
-            ...
-        };
-
-        // train their skills
-        void trainPlayerSkills(Manager managerA);
-    
-        Manager teamManager;
-    };
-
-    It does not make sense to pass in a new Manager whenever you are going to train your team players.
-    Soccer teams have ONE manager.
-
-    a much more relevant usage would be adding a member function that hires a new manager:
-
-    struct SoccerTeam
-    {
-        struct Manager
-        {
-            ...
-        };
-
-        void hireNewManager(Manager newManager);
-    
-        Manager teamManager;
-    };
-
-    We can safely assume that the 'Manager' instance that is being passed in will be replacing the current 'teamManager' variable without looking at any other code.
-    This is because the function name and function argument clearly indicate what they are/what they do.
-
-    Your function names and parameter names should make LOGICAL SENSE.
-    Readers of your code should be able to INTUITIVELY understand what your function implementations will do without actually seeing the implementations.
-
-    Keep this in mind when you define your UDTs in this project part.
-    */
 };
+}
+
+//this is what I want to see after the code is cleaned up: 
+namespace Part2Version
+{
+struct CarWash        
+{
+    int numVacuumCleaners = 3; 
+    int numEcoFriendlyCleaningSupplies = 20;     
+    float waterUsedPerWeek = 200.f;            
+    float profitPerWeek = 495.95f;               
+    int numberOfCarsServiced = 10;               
+    
+    struct Car  
+    {
+        bool isAPickupTruck = false;
+        float gasMileage = 26.2f;        
+        int year = 1985;
+        std::string manufacturer = "Toyota";
+        std::string model = "Corolla";
+
+        void fillTank(double fuelAmountInGallons = 2.0);  
+        void breakDown(std::string failureType, bool requiresTow = false);
+        int getMilesTraveledAnnually(bool includeUberLyftTrips);
+    };
+
+    void washAndWaxCar( Car car ); 
+    float chargeCustomer(float discountPercentage);
+    void detailInterior( Car car );
+    
+    Car carBeingServiced;  
+};
+}
+  /*
+    The above snippet is just an example showing you how to clean up your code.  
+    Do not put your cleaned up code into a namespace like I have done here.
+
+ 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
+    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
+    you should be able to deduce the return type of those functions based on their usage in Person::run()
+    You'll need to insert the Person struct from the video in the space below.
+ */
+
+
+
+
+
+ /*
+ 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
+    If you have 'unused parameter' warnings, you aren't using one of your function parameters in your implementation.
+    Solution: use the parameter in your implementation.
+
+    If you have 'shadows a field of <classname>' warnings, a local variable in the function has the same name as a class member.  
+    This local variable could also be a function parameter with the same name as the class member.
+    Solution: change the local variable's name so it is different from the class member variable's name.
+ 
+ 3) be sure to write the correct full qualified name for the nested type's member functions.
+ 
+ 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ if your code produces a -Wpadded warning, add '-Wno-padded' to the .replit file with the other compiler flags (-Weverything -Wno-missing-prototypes etc etc)
+ */
+
 
 
 /*
