c4ecde5cae93028dea09b611b004ec75e176e66e
pfh1976 <111196997+pfh1976@users.noreply.github.com>
2022-09-08 06:32:24 +0000

    Completed Project3 Part3

--- main.cpp
+++ main.cpp
@@ -7,21 +7,21 @@
 
  On this new branch:
 
- 0) if you opted to not fill in any of your member functions in part2, 
+ 0) if you opted to not fill in any of your member functions in part2,
     fill in ALL of your member functions
     
  1) Add a constructor for each User-Defined-Type.
     The constructor should be the first declaration in your UDT, before all member variables and member functions.
-    add a std::cout message in the constructor that prints out the name of the class being constructed.  
-    When you run your code, you'll see the order that your objects are created in the program output. 
+    add a std::cout message in the constructor that prints out the name of the class being constructed.
+    When you run your code, you'll see the order that your objects are created in the program output.
  
  2) For each User-Defined-Type:
         amend some of the member functions to print out something interesting via std::cout
  
- 3) Instantiate 1 or 2 instances of EACH of your user-defined types in the main() function.  
+ 3) Instantiate 1 or 2 instances of EACH of your user-defined types in the main() function.
     You should have at least 12 different variables declared in main(), because you have written 12 different types (including the nested types)
 
- 4) For each instantiated UDT: 
+ 4) For each instantiated UDT:
         call each of that instance's member functions.
         You're doing this to show that your code doesn't produce warnings when you call the functions that take arguments.
  
@@ -35,7 +35,7 @@
  */
 
 #include <iostream>
-namespace Example 
+namespace Example
 {
 struct UDT  // my user defined type named 'UDT'
 {
@@ -47,7 +47,7 @@ struct UDT  // my user defined type named 'UDT'
 //the function definitions are outside of the class
 UDT::UDT()
 {
-    std::cout << "UDT being constructed!" << std::endl; //1) 
+    std::cout << "UDT being constructed!" << std::endl; //1)
 }
 
 void UDT::printThing()
@@ -70,21 +70,20 @@ int main()
 
 //insert Example::main() into main() of user's repo.
 
-
-
-
 struct Guitar
 {
 
+    Guitar();
     unsigned int numberOfStrings = 6;
     std::string boydWoodType = "Mahogany";
     std::string brandName = "Jackson";
-    std::string bodyShape = "Default";  
+    std::string bodyShape = "Default";
     char size = 'M'; // S, M, L, XL
     std::string chordToPlay = "None";
 
     struct String
     {
+        String();
         float gauge = 0.0f;
         float tension = 0.0f;
         std::string coreMaterial = "Steel";
@@ -97,7 +96,7 @@ struct Guitar
     };
 
     void playChord(std::string chord = "G");
-    void PlaySolo();
+    void playSolo();
     int connectToAmplifier(std::string);
 
     void tuneString(String stringToTune, std::string key);
@@ -111,6 +110,15 @@ struct Guitar
 
 };
 
+Guitar::Guitar()
+{
+    std::cout << "Guitar being constructed!" << std::endl;
+}
+
+Guitar::String::String()
+{
+    std::cout << "String being constructed as Guitar is constructed" << std::endl;
+}
 bool Guitar::String::isCoated()
 {
     return coated;
@@ -129,11 +137,13 @@ float Guitar::String::getGauge()
 void Guitar::playChord(std::string chord)
 {
     chordToPlay = chord;
+    std::cout << "Guitar is playing " + chord << std::endl;
 }
-void Guitar::PlaySolo()
+void Guitar::playSolo()
 {
-    // do nothing
+    std::cout << "Guitar is playing an AWESOME SOLO!!!" << std::endl;
 }
+
 int Guitar::connectToAmplifier(std::string chord)
 {
     int lowPowerMode = 1;
@@ -146,6 +156,8 @@ int Guitar::connectToAmplifier(std::string chord)
 
 struct WindMill
 {
+    WindMill();
+
     unsigned int numberOfBlades = 3;
     float rotationalSpeed = 0.0f;
     int timesConnectedToGrid = 0;
@@ -158,6 +170,11 @@ struct WindMill
     bool connectToGrid();
 
 };
+
+WindMill::WindMill()
+{
+    std::cout << "WindMill being constructed!" << std::endl;
+}
 
 float WindMill::convertMechEnergyToElEnergy(float mechanicalEnergy)
 {
@@ -178,6 +195,8 @@ bool WindMill::rotateBlades(float rotSpeed)
     {
         rotationalSpeed = 0.0f;
     }
+
+    std::cout << "The blades are rotating!!" << std::endl;
     
     return isRotating;
 }
@@ -187,11 +206,14 @@ bool WindMill::connectToGrid()
     isConnectedToGrid = true;
     timesConnectedToGrid += 1;
 
+    std::cout << "WindMill connected to grid" << std::endl;
+
     return isConnectedToGrid;
 }
 
 struct MotorCycle
 {
+    MotorCycle();
     float revolutionsPerMinute = 45.0f;
     std::string color =  "blue";
     unsigned int size = 30;
@@ -203,6 +225,7 @@ struct MotorCycle
 
     struct Wheel
     {
+        Wheel();
         bool isBalanced = true;
         float rimRadius = 310.0f; // in mm
         unsigned int rimModulus = 69; // in GPa
@@ -223,6 +246,16 @@ struct MotorCycle
     Wheel backWheel;
 };
 
+MotorCycle::MotorCycle()
+{
+    std::cout << "MotorCycle being constructed!" << std::endl;
+}
+
+MotorCycle::Wheel::Wheel()
+{
+    std::cout << "Wheel being constructed!" << std::endl;
+}
+
 bool MotorCycle::Wheel::isWheelPressureLow()
 {
     bool isPressureLow = false;
@@ -240,6 +273,11 @@ void MotorCycle::Wheel::balanceWheel()
 
 bool MotorCycle::Wheel::isWheelBalanced()
 {
+    if(isBalanced)
+        std::cout << "Wheel is balanced" << std::endl;
+    else
+        std::cout << "Wheel is not balanced" << std::endl;
+    
     return isBalanced;
 }
 
@@ -271,6 +309,7 @@ void MotorCycle::getNewWheels(MotorCycle::Wheel wheelA)
 
 struct DrillSet
 {
+    DrillSet();
     std::string drillbitMaterial = "Steel";
     unsigned int drillbitDiameter = 2;
     float angularSpeed = 0.0f;
@@ -283,6 +322,11 @@ struct DrillSet
     void setRotatorySpeed(float angularSpeed = 2.0f);
     
 };
+
+DrillSet::DrillSet()
+{
+    std::cout << "DrillSet being constructed" << std::endl;
+}
 
 bool DrillSet::canDrillConcrete(std::string drillbitMaterialA)
 {
@@ -304,6 +348,7 @@ void DrillSet::setRotatorySpeed(float angularSpeedA)
 
 struct Speaker
 {
+    Speaker();
     float loudness = 90.0f;
     float resistance = 4.0f;
     float distortion = 0.1f;
@@ -316,6 +361,11 @@ struct Speaker
     void setPlaybackLevel(unsigned int volumeIndex);
 
 };
+
+Speaker::Speaker()
+{
+    std::cout << "Speaker being constructed!" << std::endl;
+}
 
 void Speaker::playSound()
 {
@@ -339,6 +389,7 @@ void Speaker::setPlaybackLevel(unsigned int volumeIndex)
 
 struct Microphone
 {
+    Microphone();
     float sensitivity = -54.0f;
     unsigned int powerSupply = 1.0f;
     float acousticOverloadPoint = 124.0f;
@@ -352,23 +403,32 @@ struct Microphone
     float getSensitivity(); // return microphone sensitivity
 };
 
+Microphone::Microphone()
+{
+    std::cout << "Microphone being constructed!" << std::endl;
+}
+
 void Microphone::captureSound()
 {
+    std::cout << "Microphone is capturing some cool sounds" << std::endl;
     isCapturing  = true;
 }
 
 void Microphone::setInputGain(float gain)
 {
+    std::cout << "Setting microphone input gain" << std::endl;
     inputGain = gain;
 }
 
 float Microphone::getSensitivity()
 {
+    std::cout << "Reading microphone sensitivity" << std::endl;
     return sensitivity;
 }
 
 struct Battery
 {
+    Battery();
     float operatingTime = 3.5f;
     std::string technologyPrinciple = "XM3";
     bool isBatteryRechargeable = true;
@@ -382,10 +442,16 @@ struct Battery
     float readCapacity(); // read the current battery's capacity
 };
 
+Battery::Battery()
+{
+    std::cout << "Battery being constructed!" << std::endl;
+}
+
 bool Battery::powerSpeaker()
 {
     isConnectedToSpeaker = true;
 
+    std::cout << "Using battery to power speaker" << std::endl;
     return isConnectedToSpeaker;
 }
 
@@ -393,16 +459,19 @@ bool Battery::powerMicrophone()
 {
     isConnectedToMic = true;
 
+    std::cout << "Using battery to power microphone" << std::endl;
     return isConnectedToMic;
 }
 
 float Battery::readCapacity()
 {
+    std::cout << "Reading battery's capacity" << std::endl;
     return operatingTime;
 }
 
 struct Antenna
 {
+    Antenna();
     float tuningRadioFrequency = 2.4f;
     unsigned int size = 1;
     std::string communicationTechnology = "COM";
@@ -415,11 +484,18 @@ struct Antenna
     float powerConsumption(Battery batteryA, float voltageInput = 1.0f);
 };
 
+Antenna::Antenna()
+{
+    std::cout << "Antenna being constructed!" << std::endl;
+}
+
 bool Antenna::setRemoteControl()
 {
     lowPowerMode = false;
     isRemoteControl = true;
 
+    std::cout << "Antenna's remote control is set" << std::endl;
+    
     return isRemoteControl;
 }
 
@@ -428,16 +504,20 @@ bool Antenna::enableAudioStreaming()
     lowPowerMode = false;
     battery.powerSpeaker();
 
+    std::cout << "Audio streaming enabled in Antenna" << std::endl;
+    
     return battery.isConnectedToSpeaker;
 }
 
 float Antenna::powerConsumption(Battery batteryA, float voltageInput)
 {
+    std::cout << "Antenna is consuming some POWER!" << std::endl;
     return batteryA.operatingTime + ( voltageInput * voltageInput );
 }
 
 struct MicroControllerUnit
 {
+    MicroControllerUnit();
     std::string microcontrollerType = "D1";
     unsigned int numberOfBits = 32;
     std::string memoryType = "RAM";
@@ -450,6 +530,11 @@ struct MicroControllerUnit
     
 };
 
+MicroControllerUnit::MicroControllerUnit()
+{
+    std::cout << "MicroControllerUnit being constructed!" << std::endl;
+}
+
 std::string MicroControllerUnit::fetchInstructionsFromMemory(std::string memoryTypeA)
 {
     std::string instruction;
@@ -461,6 +546,8 @@ std::string MicroControllerUnit::fetchInstructionsFromMemory(std::string memoryT
         instruction = "CDCD";
     else
         instruction = "Idle";
+
+    std::cout << "MCU instruction fetched from memory" << std::endl;
 
     return instruction;
 }
@@ -478,11 +565,19 @@ unsigned int MicroControllerUnit::decodeInstruction(std::string instruction)
     else
         decodedInstruction = 0;
 
+    std::cout << "MCU instruction decoded" << std::endl;
+
     return decodedInstruction;
+}
+
+void MicroControllerUnit::executeCommand(unsigned int numericCommand)
+{
+    std::cout << "The numeric command is: " << numericCommand << std::endl;
 }
 
 struct HearingAid
 {
+    HearingAid();
     Speaker speaker;
     Microphone mic;
     Antenna antenna;
@@ -495,8 +590,14 @@ struct HearingAid
     
 };
 
+HearingAid::HearingAid()
+{
+    std::cout << "HearingAid being constructed!" << std::endl;
+}
+
 void HearingAid::amplifyAudioSignals(Microphone microphoneA, float gain)
 {
+    std::cout << "HearingAid gain modified" << std::endl;
     microphoneA.setInputGain(gain);
 }
 
@@ -504,6 +605,8 @@ void HearingAid::listenToMusic()
 {
     speaker.setPlaybackLevel(8);
     speaker.playSound();
+
+    std::cout << "HearingAid is playing music. Enjoy the tune" << std::endl;
 }
 
 bool HearingAid::connectToSmartphone(std::string handShakingID)
@@ -511,8 +614,13 @@ bool HearingAid::connectToSmartphone(std::string handShakingID)
     unsigned int numericInstruction = microController.decodeInstruction(handShakingID);
 
     if(numericInstruction == 3)
+    {
+        std::cout << "HearingAid connected to smartphone" << std::endl;
         return true;
+    }
 
+    std::cout << "HearingAid NOT connected to smartphone" << std::endl;
+    
     return false;
 }
 
@@ -521,7 +629,7 @@ bool HearingAid::connectToSmartphone(std::string handShakingID)
 
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
- If you didn't already: 
+ If you didn't already:
     Make a pull request after you make your first commit
     pin the pull request link and this repl.it link to our DM thread in a single message.
 
@@ -533,8 +641,66 @@ bool HearingAid::connectToSmartphone(std::string handShakingID)
 #include <iostream>
 int main()
 {
-    Example::main();
+    //Example::main();
+    Guitar guitar;
+    WindMill windMill;
+    MotorCycle motorCycle;
+    DrillSet drillSet;
+    Speaker speaker;
+    Microphone microphone;
+    Battery battery;
+    Antenna antenna;
+    MicroControllerUnit microControllerUnit;
+    HearingAid hearingAid;
+
+    guitar.playChord("Eb");
+    guitar.playSolo();
+    guitar.connectToAmplifier("A#");
+
+    windMill.convertMechEnergyToElEnergy(24.56f);
+    windMill.rotateBlades(64.0f);
+    windMill.connectToGrid();
+
+    motorCycle.accelerate(2.0f);
+    motorCycle.readGasTankLevel();
+    motorCycle.cruise(34.5f);
+    motorCycle.getNewWheels(motorCycle.frontWheel);
     
+    drillSet.canDrillConcrete("Steel");
+    drillSet.setDirectionOfRotation(-1);
+    drillSet.setRotatorySpeed(45.0f);
+    
+    speaker.playSound();
+    speaker.powerConsumption();
+    speaker.setPlaybackLevel(10);
+    
+    microphone.captureSound();
+    microphone.getSensitivity();
+    microphone.setInputGain(3.5f);
+    
+    battery.powerMicrophone();
+    battery.powerSpeaker();
+    battery.readCapacity();
+    
+    antenna.powerConsumption(battery);
+    antenna.enableAudioStreaming();
+    antenna.setRemoteControl();
+    
+    std::string instructionFromMCU = microControllerUnit.fetchInstructionsFromMemory("RAM");
+    microControllerUnit.decodeInstruction(instructionFromMCU);
+    microControllerUnit.executeCommand(0);
+    
+    hearingAid.amplifyAudioSignals(microphone, 2.0f);
+    hearingAid.connectToSmartphone();
+    hearingAid.listenToMusic();
+    
+    std::cout << "Is the wind mill connected to the grid? " << (windMill.isConnectedToGrid == 0 ? "Yes" : "No") << "\n";
+    std::cout << "The drill set brand name is: " + drillSet.brandName << std::endl;
+    std::cout << "The Mic input gain is set to " << microphone.inputGain << std::endl;
+    std::cout << "What is the battery tech. principle? " << battery.technologyPrinciple << std::endl;
+    std::cout << "The current instruction from the MCU is " << instructionFromMCU << std::endl;
+    std::cout << "Is the battery of the hearing aid rechargeable? " << (hearingAid.battery.isBatteryRechargeable ? "Yes" : "No") << std::endl;
     
     std::cout << "good to go!" << std::endl;
 }
+
